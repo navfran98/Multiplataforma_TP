9febd08e7208d1daa13fde6ffe1c60c55b6979e5
Franco Navarro <frnavarro@itba.edu.ar>
2023-08-10 17:17:54 -0300

    Card Voluntariado

--- multi_tp/lib/main.dart
+++ multi_tp/lib/main.dart
@@ -31,95 +31,112 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+//TODO: falta agregar "roboto" como font a los text
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Test"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Material(
+          elevation: 10,
+          child: Container(
+            width: 328,
+            height: 234,
+            child: Column(
+              children: [
+                Image.asset(
+                  'images/Landscape-Color.jpg',
+                  fit: BoxFit.fill
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        alignment: AlignmentDirectional.centerStart,
+                        width: 232,
+                        height: 72,
+                        child:Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "ACCION SOCIAL",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 10, letterSpacing: 1.5, color: Color.fromRGBO(102, 102, 102, 1)
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            const Text(
+                              "Un Techo para mi Pais",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400, fontSize: 16, letterSpacing: 0.15, color: Color.fromRGBO(25, 25, 25, 1)
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            SizedBox(height: 4,),
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                              height: 28,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4.0),
+                                color: const Color.fromRGBO(202, 229 , 251, 1),
+                              ),
+                              child: const Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "Vacantes:",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w400, fontSize: 12, letterSpacing: 0.4, color: Color.fromRGBO(25, 25, 25, 1)
+                                    ),
+                                  ),
+                                  SizedBox(width: 8,),
+                                  Row(
+                                      children: [
+                                        Icon(Icons.person, color: Color.fromRGBO(13, 71, 161, 1),size: 20,),
+                                        Text(
+                                          "10",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w400, fontSize: 16, letterSpacing: 0.15, color: Color.fromRGBO(13, 71, 161, 1)
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                ]
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 24,
+                        width: 64,
+                        child: const Row(
+                          children: [
+                            Icon(Icons.favorite_border, size: 24, color: Colors.green,),
+                            SizedBox(width: 16,),
+                            Icon(Icons.location_pin, size:24, color: Colors.green,)
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
